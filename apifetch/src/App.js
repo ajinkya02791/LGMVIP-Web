92ff691b847f8dfd05ceff451315d3788910c8bb
ajinkya <77152422+ajinkya02791@users.noreply.github.com>
2025-08-23 18:06:19 +0530

    change the how users data is used and added throw and catch block

--- apifetch/src/App.js
+++ apifetch/src/App.js
@@ -5,39 +5,43 @@ export default function App() {
   const [users, setUsers] = useState([]);
 
   const fetchUserData = async () => {
-    const response = await fetch("https://reqres.in/api/users?page=1");
-    const responseJson = await response.json();
-    setUsers(responseJson);
+    try {
+      const response = await fetch("https://reqres.in/api/users?page=1");
+      if (!response.ok) {
+        throw new Error("HTTP error! status: " + response.status);
+      }
+      const responseJson = await response.json();
+      setUsers(responseJson.data); // store only the array of users
+    } catch (err) {
+      console.error("Fetch failed ❌:", err);
+      alert("Fetch failed ❌: " + err.message);
+    }
   };
-
-  const cols =
-    users.data &&
-    users.data.map((user, index) => (
-      <div className="col">
-        <div key={index} className="card">
-          <div>
-            <img className="img" src={user.avatar} alt="user-avatar" />
-            <div className="card-body">
-              <h5>
-                {" "}
-                {user.first_name} {user.last_name}{" "}
-              </h5>
-              <h5>User Id: {user.id}</h5>
-              <h5> E-mail : {user.email} </h5>
-              <hr />
-            </div>
-          </div>
-        </div>
-      </div>
-    ));
 
   return (
     <div className="App">
-      <h1>Hello Everyone</h1>
+      <h1>Hello Everyone.</h1>
       <button onClick={fetchUserData} id="getUser">
         Get Users
       </button>
-      <div className="rows"> {users.data && cols} </div>{" "}
+
+      <div className="rows">
+        {users.map((user) => (
+          <div key={user.id} className="col">
+            <div className="card">
+              <img className="img" src={user.avatar} alt="user-avatar" />
+              <div className="card-body">
+                <h5>
+                  {user.first_name} {user.last_name}
+                </h5>
+                <h5>User Id: {user.id}</h5>
+                <h5>E-mail : {user.email}</h5>
+                <hr />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
